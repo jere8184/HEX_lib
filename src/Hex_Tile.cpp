6432dc4c45a0e3b7aed89ae0d5fa3a73e3d1f0a0
jere8184 <jeremiahmorgan.bham@outlook.com>
2023-04-07 02:10:30 +0100

    traverse hex corners clockwise

--- src/Hex_Tile.cpp
+++ src/Hex_Tile.cpp
@@ -5,7 +5,7 @@
 
 Point Hex_Tile::Get_Corner_Point(int corner_index)
 {
-    double angle_to_corner = corner_index * (60 - rotation);
+    double angle_to_corner = corner_index * (-60 - rotation); //-60 as we want to traverse hex clockwise
     double angle_in_radians = angle_to_corner * M_PI / 180;
     return Point(center.x + radius * cos(angle_in_radians), center.y + radius * sin(angle_in_radians));
 }
