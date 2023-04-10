75fd2e5e50c85065b78e993026678eb53ff8032e
jere8184 <jeremiahmorgan.bham@outlook.com>
2023-04-10 18:26:58 +0100

    use conan to import sfml and draw hex

--- scripts/readme.txt
+++
@@ -1,4 +0,0 @@
-this file will contain scripts which can be used to interface with the project.
-If I a script is not intended to be used by end user, it will be place in a utility sub dir.
-
-My current plan is to use python (cross platform)
--- src/Hex_Tile.cpp
+++ src/Hex_Tile.cpp
@@ -1,11 +1,23 @@
 #define _USE_MATH_DEFINES
 #include "Hex_Tile.hpp"
 #include <math.h>
+#include <vector>
 
 
-Point Hex_Tile::Get_Corner_Point(int corner_index)
+Point Hex_Tile::Get_Corner(int corner_index)
 {
     double angle_to_corner = corner_index * (60 - rotation);
     double angle_in_radians = angle_to_corner * M_PI / 180;
     return Point(center.x + radius * cos(angle_in_radians), center.y + radius * sin(angle_in_radians));
+}
+
+std::vector<Point> Hex_Tile::Get_All_Corners()
+{
+    std::vector<Point> vertices = {};
+    for(int corner_index = 0; corner_index < 6; corner_index++)
+    {
+        Point point = Get_Corner(corner_index);
+        vertices.push_back(point);
+    }
+    return vertices;
 }
